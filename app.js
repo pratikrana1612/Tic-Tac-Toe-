122c838370d4ccb63b7505e4e096a45d23d8b245
Pratik <ranapratik1612@gmail.com>
2023-06-12 15:28:06 +0530

    SCORE FUNCTIONLITY ADDED

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const section = document.getElementById("main_section");
 const h1 = document.getElementById("chance");
 const line = document.querySelector(".line");
 const backDrop = document.querySelector("#backdrop");
+const el_score1 = document.getElementById(`user1-score`);
+const el_score2 = document.getElementById(`user2-score`);
+let [user1Score, user2Score] = intialSetup();
+// let user1Score = localStorage.getItem("user1") || 0;
+// let user2Score = localStorage.getItem("user2") || 0;
+
 let sings = new Array(9);
 let turn = "user1";
 let possibleWins = [
@@ -14,6 +20,20 @@ let possibleWins = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+localStorage.setItem("user1", user1Score);
+localStorage.setItem("user2", user2Score);
+
+ function intialSetup () {
+  if (!localStorage.getItem("user1") || !localStorage.getItem("user2")) {
+    localStorage.setItem("user1", 0);
+    localStorage.setItem("user2", 0);
+  }
+  let user1Score = localStorage.getItem("user1") || 0;
+  let user2Score = localStorage.getItem("user2") || 0;
+  el_score1.textContent = user1Score;
+  el_score2.textContent = user2Score;
+  return [user1Score, user2Score];
+};
 const gameOver = (winner) => {
   section.removeEventListener("click", ClickHandler);
   backDrop.classList.add("backdrop");
@@ -48,6 +68,16 @@ const drawTheLine = (winningPair) => {
       possibleWins[7] === winningPair ? "rotate(135deg)" : "rotate(225deg)";
   }
 };
+const scoreUpdate = (winner) => {
+  if (!winner) {
+    return;
+  }
+  const key = winner;
+  let score = winner === "user1" ? ++user1Score : ++user2Score;
+  localStorage.setItem(key, score);
+  el_score1.textContent = user1Score;
+  el_score2.textContent = user2Score;
+};
 const WinnerChecker = function (Player) {
   for (const array of possibleWins) {
     if (
@@ -58,6 +88,7 @@ const WinnerChecker = function (Player) {
       console.log(`${Player} is the winer`);
       gameOver(Player);
       drawTheLine(array);
+      scoreUpdate(Player);
       array.forEach(
         (el) =>
           (document.getElementById(el).style["backgroundColor"] =
